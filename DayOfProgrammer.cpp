58a6fe6887af07209651feda4e004e43e822f994
naveennavee <53909065+naveennavee@users.noreply.github.com>
2020-01-23 19:00:01 +0530

    Add files via upload

---
+++ DayOfProgrammer.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<stdio.h>
+#include<math.h>
+using namespace std;
+int main(){
+    int y;
+    cin>>y;
+    
+    if(y>=1919){
+    if( (y%4)==0 && ((y%100)!=0) )
+        {
+           cout<<"12.09."<<y;
+    }
+    else if((((y%400)==0) && (y%100)==0) && (y%4)==0)
+    {
+        cout<<"12.09."<<y;
+    }
+    else{            
+        cout<<"13.09."<<y;  
+          }
+   } 
+   else if(y<1918){
+            if((y%4)==0 ){
+                cout<<"12.09."<<y;
+          }
+          else{
+              cout<<"13.09."<<y;
+          }
+    }
+    else{
+        cout<<"26.09."<<y;
+    }
+    }
+
