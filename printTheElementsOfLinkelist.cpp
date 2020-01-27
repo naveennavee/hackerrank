f7af2da47aed1014e0376d3260b2bdf7f05809eb
naveennavee <53909065+naveennavee@users.noreply.github.com>
2020-01-27 22:48:15 +0530

    Add files via upload

---
+++ printTheElementsOfLinkelist.cpp
@@ -0,0 +1,16 @@
+
+
+// Complete the printLinkedList function below.
+
+
+ // For your reference
+
+void printLinkedList(SinglyLinkedListNode* head) {
+    while(head!=NULL){
+        cout<<head->data<<"\n";
+        head =head->next;
+    }
+
+
+}
+
